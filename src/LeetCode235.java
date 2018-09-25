272b857dc4e842f4dd92b8ef171a6de950b76bb2
weizb <wzb19960208@outlook.com>
2018-09-25 19:21:03 +0800

    'AC235'

---
+++ src/LeetCode235.java
@@ -0,0 +1,18 @@
+public class LeetCode235 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(root==null||root==p||root==q)
+            return root;
+
+        if (p.val<root.val&&q.val<root.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }else if(p.val>root.val&&q.val>root.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }else {
+            return root;
+        }
+
+    }
+
+}
---
+++ src/LeetCode400.java
@@ -0,0 +1,2 @@
+public class LeetCode400 {
+}
